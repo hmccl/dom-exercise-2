d7ba9a6e747ff9976fdd88037566d24342143817
helio <heliomccl@gmail.com>
2025-06-15 22:28:49 -0300

    Adiciona questão 4

---
+++ ex04/script.js
@@ -0,0 +1,20 @@
+function displayImage() {
+  document.getElementById('imgButton').addEventListener('click', () => {
+    let selectedImage = document.getElementById('imageInput').files[0];
+    if (selectedImage) {
+      let figure = document.getElementById('result'); 
+  
+      let img = document.createElement('img');
+      img.src = URL.createObjectURL(selectedImage);
+      img.alt = 'Imagem carregada pelo usuário';
+
+      let figcaption = document.createElement('figcaption');
+      figcaption.innerText = 'Imagem carregada pelo usuário.';
+
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+    }
+  });
+}
+
+displayImage();
