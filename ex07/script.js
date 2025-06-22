a7955ec27ca2ee47a822d5deec684787673c5dc5
helio <heliomccl@gmail.com>
2025-06-22 15:54:58 -0300

    Remove array auxiliar

--- ex07/script.js
+++ ex07/script.js
@@ -12,10 +12,10 @@ function remHashtag() {
   document.getElementById('remTagButton').addEventListener('click', () => {
     let hashtagSelect = document.getElementById('hashtagSelect').selectedOptions;
 
+    // Array para hashtags selecionadas
     let remHashtagArray = [];
     for (let i = 0; i < hashtagSelect.length; i++) {
       remHashtagArray.push(hashtagSelect[i].id);
-      // TODO remover de hashtagArray
     }
 
     for (let i = 0; i < remHashtagArray.length; i++) {
@@ -31,9 +31,15 @@ function addHashtag() {
   document.getElementById('addTagButton').addEventListener('click', () => {
     let hashtagInput = document.getElementById('hashtagInput').value.trim();
 
-    if (hashtagInput && !(hashtagArray.includes(hashtagInput)) && hashtagInput.length > 2 && hashtagArray.length < 5) {
-      hashtagArray.push(hashtagInput);
+    // Array para hashtags já adicionadas
+    let hashtagSelect = document.getElementById('hashtagSelect').options;
+    let hashtagArray = [];
+    for (let i = 0; i < hashtagSelect.length; i++) {
+      hashtagArray.push(hashtagSelect[i].id);
+    }
 
+    // Condições para adicionar hashtags
+    if (hashtagInput && !(hashtagArray.includes(hashtagInput)) && hashtagInput.length > 2 && hashtagArray.length < 5) {
       let newHashtagOpt = document.createElement('option');
       newHashtagOpt.innerText = hashtagInput;
       newHashtagOpt.id = hashtagInput;
@@ -46,8 +52,6 @@ function addHashtag() {
     }
   });
 }
-
-let hashtagArray = [];
 
 addHashtag();
 remHashtag();
